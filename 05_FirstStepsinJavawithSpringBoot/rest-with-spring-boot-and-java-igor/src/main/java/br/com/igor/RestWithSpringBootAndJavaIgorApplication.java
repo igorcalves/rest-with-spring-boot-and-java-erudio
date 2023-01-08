6ead2bafcd3144bec9c233efc991188e7e3a4762
igor alves <igor193@live.com>
2023-01-08 07:26:38 -0300

    First steps in java with spring boot 3

---
+++ 05_FirstStepsinJavawithSpringBoot/rest-with-spring-boot-and-java-igor/src/main/java/br/com/igor/RestWithSpringBootAndJavaIgorApplication.java
@@ -0,0 +1,13 @@
+package br.com.igor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaIgorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootAndJavaIgorApplication.class, args);
+	}
+
+}
