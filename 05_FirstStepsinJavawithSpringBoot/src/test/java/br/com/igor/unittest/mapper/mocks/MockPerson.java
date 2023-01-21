239d9b573f6b93ae94e41b24e26febd9229235d3
igor alves <igor193@live.com>
2023-01-20 21:32:31 -0300

    21:32

--- 05_FirstStepsinJavawithSpringBoot/src/test/java/br/com/igor/unittest/mapper/mocks/MockPerson.java
+++ 05_FirstStepsinJavawithSpringBoot/src/test/java/br/com/igor/unittest/mapper/mocks/MockPerson.java
@@ -36,9 +36,9 @@ public class MockPerson {
 		Person person = new Person();
 		person.setAddress("Addres Test" + number);
 		person.setFirstName("First Name Test" + number);
+		person.setLastName("Last Name Test" + number);
 		person.setGender(((number % 2) == 0) ? "Male" : "Female");
 		person.setId(number.longValue());
-		person.setLastName("Last Name Test" + number);
 		return person;
 	}
 
