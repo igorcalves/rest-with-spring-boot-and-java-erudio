e91be85f66ce3fcf5b7c5bd15a7afa68a36f10a9
igor alves <igor193@live.com>
2023-03-14 15:57:51 -0300

    l

--- 05_FirstStepsinJavawithSpringBoot/src/main/java/br/com/igor/model/User.java
+++ 05_FirstStepsinJavawithSpringBoot/src/main/java/br/com/igor/model/User.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
